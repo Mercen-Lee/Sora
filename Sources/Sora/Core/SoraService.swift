dc8736e24841973e12a58a2b5f60ff053d257fbb
Mercen <mercen@mercen.net>
2024-01-26 15:03:04 +0900

    DELETE: Default path initializer

--- Sources/Sora/Core/SoraService.swift
+++ Sources/Sora/Core/SoraService.swift
@@ -32,28 +32,16 @@ public protocol SoraService {
     var endpoint: URL { get }
     
     /// The URL path can be appended to the `endpoint`.
-    var path: [String] { get set }
+    var path: [String] { get }
     
     /// The URL composed with the `endpoint` and the `path`
     var url: URL { get }
     
     /// The `Interceptor` of `Alamofire`.
     var interceptor: Interceptor { get }
-    
-    /// Default Initializer
-    init()
-    
-    /// An initializer with the `Path`
-    init(path: String...)
 }
 
 public extension SoraService {
-    
-    /// The URL path can be appended to the `endpoint`.
-    var path: [String] {
-        get { .init() }
-        set { }
-    }
     
     /// The URL composed with the `endpoint` and the `path`
     var url: URL {
@@ -64,13 +52,4 @@ public extension SoraService {
     
     /// The `Interceptor` of Alamofire.
     var interceptor: Interceptor { .init() }
-    
-    /// Default Initializer
-    init() { self.init() }
-    
-    /// An initializer with the `Path`
-    init(path: String...) {
-        self.init()
-        self.path = path
-    }
 }
--- Tests/SoraTests/SoraRequestTests.swift
+++ Tests/SoraTests/SoraRequestTests.swift
@@ -4,12 +4,13 @@ import Alamofire
 
 struct TestService: SoraService {
     
-    let endpoint: URL = URL(string: "https://example.com/")!
+    let endpoint: URL = URL(string: "https://reqres.in/api/")!
+    let path: [String]
 }
 
 struct PostExample: SoraRequest {
     
-    let service = TestService(path: "hi")
+    let service = TestService(path: ["users"])
     let method: HTTPMethod = .post
     
     struct Body: Encodable {
@@ -24,7 +25,12 @@ final class SoraTests: XCTestCase {
     func testPostRequest() throws {
         AF.request(PostExample())
             .responseData { response in
-                
+                switch response.result {
+                case .success(let data):
+                    print(String(decoding: data, as: UTF8.self))
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
             }
     }
 }
